e0cc3abb9490783a19454c5484511721c5b84f7e
Vien Dinh <maxvien@gmail.com>
2019-03-21 23:04:07 +0700

    Add Bootstrap

--- static/assets/main.js
+++ static/assets/main.js
@@ -1,2 +1,9 @@
+/* Component: .m-footer
+--------------------------------------------------*/
+$('.m-footer').exists(function() {});
+
+/* Component: .m-header
+--------------------------------------------------*/
+$('.m-header').exists(function() {});
 
 //# sourceMappingURL=maps/main.js.map
